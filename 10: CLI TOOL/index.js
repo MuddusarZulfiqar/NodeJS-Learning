7ede26083f3b5f3a3b157bcb66354bb3fa82cc63
Muddusar <muddusar.zulfiqar@purelogics.net>
2023-02-26 01:30:05 +0500

    set cli tool

--- 10: CLI TOOL/index.js
+++ 10: CLI TOOL/index.js
@@ -3,22 +3,16 @@
 // create cli tool get the pokemon name and return the pokemon 5 moves
 
 const readline = require("readline");
-const axios = require("axios");
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question("Please enter the Pokemon name: ", async (name) => {
-  const pokemon = name.toLowerCase();
-  const response = await axios.get(
-    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-  );
-  const data = await response.json();
-  const moves = data.moves.map((move) => move.move.name);
+rl.question("Please enter the Pokemon name: ", (name) => {
   // Get the pokemon 5 moves from API here
 
-  console.log(`The 5 moves of ${name} are:`, moves);
+  console.log(`The 5 moves of ${name} are:`);
 
   // Log out the pokemon 5 moves here
 
